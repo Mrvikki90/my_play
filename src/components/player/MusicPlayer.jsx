4728f59c51dcf4b959a236c53c0b0276139f0be3
vikas singh <90423539+Mrvikki90@users.noreply.github.com>
2025-03-24 17:55:22 +0530

    Update MusicPlayer.jsx

--- src/components/player/MusicPlayer.jsx
+++ src/components/player/MusicPlayer.jsx
@@ -4,20 +4,21 @@ import Styles from "./css/musicPlayer.module.css";
 import Track from "./Track";
 
 const MusicPlayer = (song) => {
+  console.log("song==========>>", song?.song?.song?.downloadUrl);
+
   return (
     <>
-      {/* <Track activeSong={song && song.song && song.song.song} /> */}
       <div className={`${Styles.music_player}`}>
         <AudioPlayer
           autoPlay={true}
           autoPlayAfterSrcChange={true}
           src={
-            song &&
-            song.song &&
-            song.song.song &&
-            song.song.song.downloadUrl[4].link
+            song?.song?.song?.downloadUrl?.[4]?.url ||
+            song?.song?.song?.downloadUrl?.[3]?.url ||
+            song?.song?.song?.downloadUrl?.[2]?.url ||
+            song?.song?.song?.downloadUrl?.[1]?.url ||
+            song?.song?.song?.downloadUrl?.[0]?.url
           }
-          // onPlay={(e) => console.log("onPlay")}
           style={{
             background:
               "linear-gradient(to bottom right, rgba(255, 255, 255, 0.1), #2a2a80)",
