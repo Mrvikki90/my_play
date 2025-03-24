af1f58dfc086b21917ceccd26a3dc826231fc395
vikas singh <90423539+Mrvikki90@users.noreply.github.com>
2025-03-24 17:55:44 +0530

    Update ArtistCard.jsx

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
 
+
   return (
     <div
       className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
@@ -11,7 +12,8 @@ const ArtistCard = ({ track }) => {
     >
       <img
         alt="song_img"
-        src={track?.image[1]?.link}
+        src={track?.image?.[2]?.url || track?.image?.[1]?.url || track?.image?.[0]?.url }
+
         className="w-full h-56 rounded-lg"
       />
       <p className="mt-4 font-semibold text-lg text-white truncate">
